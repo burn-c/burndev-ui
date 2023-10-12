6eaba4715b943b6cc4f538c847106ba9a4009e60
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-12 02:12:02 -0300

    config: adds eslint-config on packages

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,3 @@
-import {colors} from '@burndev-ui/tokens'
+import { colors } from '@burndev-ui/tokens'
+
+console.log(colors)
--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -7,4 +7,3 @@ export * from './fonts'
 export * from './font-sizes'
 export * from './font-weights'
 export * from './line-heights'
-
