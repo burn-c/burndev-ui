1692adb85fdc520f63b2ccb2c202c5a2a8458872
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-13 00:10:02 -0300

    config: react developer dependecies

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@burndev-ui/tokens'
-
-console.log(colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@burndev-ui/tokens'
+
+export function App() {
+  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+}
