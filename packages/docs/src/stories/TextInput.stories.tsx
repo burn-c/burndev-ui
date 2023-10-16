c5469be96706aac1fd7de6ab54b6d1791e74f47e
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-16 05:36:17 -0300

    feat(comp-stories): adds TextInput

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,42 @@
+import { Box, Text, TextInput, TextInputProps } from '@burndev-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/TextInput',
+  component: TextInput,
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$2',
+          }}
+        >
+          <Text size="sm">Email address</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps } from '@stitches/react'
+import { Input, Prefix, TextInputContainer } from './styles'
+
+export interface TextInputProps extends ComponentProps<typeof Input> {
+  prefix?: string
+}
+
+export function TextInput({ prefix, ...rest }: TextInputProps) {
+  return (
+    <TextInputContainer>
+      {!!prefix && <Prefix>{prefix}</Prefix>}
+      <Input {...rest} />
+    </TextInputContainer>
+  )
+}
