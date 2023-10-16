c0947a6c6e16c9142b7def829bc2c7667538ee19
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-15 21:35:59 -0300

    feat(comp-stories): adds Heading

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,27 @@
+import { Heading, HeadingProps } from '@burndev-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'H2 default Heading',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 custom Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão o heading sempre será um `h2`, mas podemos alterar seu tipo com a proprieade `as` para qualquer elemento de texto do `html`.',
+      },
+    },
+  },
+}
