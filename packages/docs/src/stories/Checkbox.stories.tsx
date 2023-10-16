9e2775a9d96e18f7f4cb0295ac37bd958ba6f50b
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-16 06:03:59 -0300

    feat(comp-stories): adds Checkbox

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,31 @@
+import { Box, Text, Checkbox, CheckboxProps } from '@burndev-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            gap: '$2',
+          }}
+        >
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {}
+
+export const Disabled: StoryObj<CheckboxProps> = {
+  args: {
+    disabled: true,
+  },
+}
