84e0518414c7e72e04262c299f6a3640780348af
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-15 21:21:13 -0300

    type(comp): improvements Box

--- packages/react/src/components/Box.tsx
+++ packages/react/src/components/Box.tsx
@@ -1,6 +1,7 @@
-import { ComponentProps } from 'react'
+import { ElementType } from 'react'
 
 import { styled } from '../styles'
+import { ComponentProps } from '@stitches/react'
 
 export const Box = styled('div', {
   padding: '$4',
@@ -9,4 +10,6 @@ export const Box = styled('div', {
   border: '1px solid $gray600',
 })
 
-export type BoxProps = ComponentProps<typeof Box>
+export interface BoxProps extends ComponentProps<typeof Box> {
+  as?: ElementType
+}
