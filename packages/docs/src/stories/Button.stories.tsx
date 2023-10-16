921a4e4049acb20b3e86c1cc8a14b88795fff002
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-16 05:11:41 -0300

    feat(comp-stories): adds Button

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,53 @@
+import { Button, ButtonProps } from '@burndev-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { ArrowRight } from 'phosphor-react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Default Button',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Secondary Button',
+  },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+    children: 'Small',
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+    children: (
+      <>
+        Próximo passo
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+    children: 'Disabled',
+  },
+}
